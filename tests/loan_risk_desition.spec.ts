0fb221ebf42e503b6d5d8f99a780d00e0b28c98a
Asty <asty@itdevels.eu>
2025-07-06 16:12:42 +0300

    HW10 - loan tests using DTO pattern and write comment

--- tests/loan_risk_desition.spec.ts
+++ tests/loan_risk_desition.spec.ts
@@ -2,7 +2,7 @@ import {APIResponse, expect, test} from '@playwright/test'
 import {ApplicationDto} from "./dto/applicationDto";
 import {StatusCodes} from "http-status-codes";
 
-test ('post credit info with incorrect data should receive code 200', async ({ request }) => {
+test ('post credit info with negative decision should receive code 200', async ({ request }) => {
     //prepare request body with dto pattern and very high risk
     const requestBody = new ApplicationDto(100, 0, 17, true, 1000, 12);
     const response:APIResponse = await request.post('https://backend.tallinn-learning.ee/api/loan-calc/decision',{
@@ -14,7 +14,7 @@ test ('post credit info with incorrect data should receive code 200', async ({ r
     expect(response.status()).toBe(StatusCodes.OK )
 });
 
-test ('post credit info with correct data should receive code 200', async ({ request })  => {
+test ('post credit info with positive decision should receive code 200', async ({ request })  => {
 // prepare request body with dto pattern and medium risk
     const requestBody = new ApplicationDto(20000, 0, 30, true, 500, 6);
     const response:APIResponse = await request.post('https://backend.tallinn-learning.ee/api/loan-calc/decision',{
@@ -25,7 +25,7 @@ test ('post credit info with correct data should receive code 200', async ({ req
     expect(response.status()).toBe(StatusCodes.OK )
 });
 
-test ('post credit info with data should receive code 200', async ({ request }) => {
+test ('post credit info with positive decision should receive code 200', async ({ request }) => {
     // prepare request body with dto pattern and low risk
     const requestBody = new ApplicationDto(20000, 0, 30, true, 500, 12);
     const response:APIResponse = await request.post('https://backend.tallinn-learning.ee/api/loan-calc/decision',{
