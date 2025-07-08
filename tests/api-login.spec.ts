cb5a3cc1297f75f06e73016b9cd66c4595afb8d5
Asty <asty@itdevels.eu>
2025-07-08 21:13:57 +0300

    hw11 fix pretifier

--- tests/api-login.spec.ts
+++ tests/api-login.spec.ts
@@ -1,98 +1,97 @@
-import {APIResponse, expect, test} from '@playwright/test'
+import { APIResponse, expect, test } from '@playwright/test'
 
 import { StatusCodes } from 'http-status-codes'
-import {OrderDto} from "./dto/orderDto";
-import {LoginDto} from "./dto/login-dto";
+import { OrderDto } from './dto/orderDto'
+import { LoginDto } from './dto/login-dto'
 
 // JWT regex
-const jwtRegex = /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/;
+const jwtRegex = /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
 
-test("api login with correct credentials using loginDto and verify JWT", async ({request}) => {
-    // prepare login request body
-    const requestBody = LoginDto.createLoginWithCorrectData()
-    // make login request
-    const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
-        data: requestBody
-    })
+test('api login with correct credentials using loginDto and verify JWT', async ({ request }) => {
+  // prepare login request body
+  const requestBody = LoginDto.createLoginWithCorrectData()
+  // make login request
+  const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
+    data: requestBody,
+  })
 
-    // retrieve jwt token
-    const jwtValue = response.text();
+  // retrieve jwt token
+  const jwtValue = response.text()
 
-    console.log('JWT token:', jwtValue)
+  console.log('JWT token:', jwtValue)
 
-    expect(response.status()).toBe(StatusCodes.OK)
-    expect(jwtValue).toMatch(jwtRegex)
+  expect(response.status()).toBe(StatusCodes.OK)
+  expect(jwtValue).toMatch(jwtRegex)
 })
 
-test("login and create order with correct credentials and order data", async ({request}) => {
-    // login student with correct data using LoginDto
-    const requestBody = LoginDto.createLoginWithCorrectData()
-    const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
-        data: requestBody
-    })
+test('login and create order with correct credentials and order data', async ({ request }) => {
+  // login student with correct data using LoginDto
+  const requestBody = LoginDto.createLoginWithCorrectData()
+  const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
+    data: requestBody,
+  })
 
-    // retrieve jwt token
-    const jwt = response.text();
-    // prepare new order request body
-    const orderBody = new OrderDto('Open', 0, 'new', '456677', 'new');
-    // make request to create new order with jwt token
-    const orderResponse = await request.post('https://backend.tallinn-learning.ee/order', {
-        headers: {
-            Authorization: `Bearer ${jwt}`,
-        },
-        data: orderBody
-    })
+  // retrieve jwt token
+  const jwt = response.text()
+  // prepare new order request body
+  const orderBody = new OrderDto('Open', 0, 'new', '456677', 'new')
+  // make request to create new order with jwt token
+  const orderResponse = await request.post('https://backend.tallinn-learning.ee/order', {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+    data: orderBody,
+  })
 
-    // parse order response
-    const orderResponseBody = await orderResponse.json();
+  // parse order response
+  const orderResponseBody = await orderResponse.json()
 
-    console.log('orderResponse status:', orderResponse.status())
-    console.log('orderResponse:', orderResponseBody)
-    expect(orderResponse.status()).toBe(StatusCodes.OK)
-    expect(orderResponseBody.id).toBeGreaterThan(0)
-
+  console.log('orderResponse status:', orderResponse.status())
+  console.log('orderResponse:', orderResponseBody)
+  expect(orderResponse.status()).toBe(StatusCodes.OK)
+  expect(orderResponseBody.id).toBeGreaterThan(0)
 })
 
-test("make request with incorrect HTTP method", async ({request}) => {
-    // login student with correct data using LoginDto
-    const requestBody = LoginDto.createLoginWithCorrectData()
-    const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
-        data: requestBody
-    })
+test('make request with incorrect HTTP method', async ({ request }) => {
+  // login student with correct data using LoginDto
+  const requestBody = LoginDto.createLoginWithCorrectData()
+  const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
+    data: requestBody,
+  })
 
-    // retrieve jwt token
-    const jwt = response.text();
-    // prepare new order request body
-    const orderBody = new OrderDto('Open', 0, 'new', '456677', 'new');
-    // make request to create new order with jwt token and invalid HTTP method 'patch'
-    const orderResponse = await request.patch('https://backend.tallinn-learning.ee/order', {
-        headers: {
-            Authorization: `Bearer ${jwt}`,
-        },
-        data: orderBody
-    })
+  // retrieve jwt token
+  const jwt = response.text()
+  // prepare new order request body
+  const orderBody = new OrderDto('Open', 0, 'new', '456677', 'new')
+  // make request to create new order with jwt token and invalid HTTP method 'patch'
+  const orderResponse = await request.patch('https://backend.tallinn-learning.ee/order', {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+    data: orderBody,
+  })
 
-    expect(orderResponse.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+  expect(orderResponse.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
 })
 
-test("make request with incorrect order body", async ({request}) => {
-    // login student with correct data using LoginDto
-    const requestBody = LoginDto.createLoginWithCorrectData()
-    const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
-        data: requestBody
-    })
+test('make request with incorrect order body', async ({ request }) => {
+  // login student with correct data using LoginDto
+  const requestBody = LoginDto.createLoginWithCorrectData()
+  const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
+    data: requestBody,
+  })
 
-    // retrieve jwt token
-    const jwt = response.text();
-    // prepare new order request body
-    const orderBody = {"name": "test"};
-    // make request to create new order with jwt token and invalid order body
-    const orderResponse = await request.post('https://backend.tallinn-learning.ee/order', {
-        headers: {
-            Authorization: `Bearer ${jwt}`,
-        },
-        data: orderBody
-    })
+  // retrieve jwt token
+  const jwt = response.text()
+  // prepare new order request body
+  const orderBody = { name: 'test' }
+  // make request to create new order with jwt token and invalid order body
+  const orderResponse = await request.post('https://backend.tallinn-learning.ee/order', {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+    data: orderBody,
+  })
 
-    expect(orderResponse.status()).toBe(StatusCodes.BAD_REQUEST)
+  expect(orderResponse.status()).toBe(StatusCodes.BAD_REQUEST)
 })
--- tests/loan_risk_desition.spec.ts
+++ tests/loan_risk_desition.spec.ts
@@ -1,37 +1,46 @@
-import {APIResponse, expect, test} from '@playwright/test'
-import {ApplicationDto} from "./dto/applicationDto";
-import {StatusCodes} from "http-status-codes";
+import { APIResponse, expect, test } from '@playwright/test'
+import { ApplicationDto } from './dto/applicationDto'
+import { StatusCodes } from 'http-status-codes'
 
-test ('post credit info with negative decision should receive code 200', async ({ request }) => {
-    //prepare request body with dto pattern and very high risk
-    const requestBody = new ApplicationDto(100, 0, 17, true, 1000, 12);
-    const response:APIResponse = await request.post('https://backend.tallinn-learning.ee/api/loan-calc/decision',{
-        data: requestBody
-    });
+test('post credit info with negative decision should receive code 200', async ({ request }) => {
+  //prepare request body with dto pattern and very high risk
+  const requestBody = new ApplicationDto(100, 0, 17, true, 1000, 12)
+  const response: APIResponse = await request.post(
+    'https://backend.tallinn-learning.ee/api/loan-calc/decision',
+    {
+      data: requestBody,
+    },
+  )
 
-    console.log('response status:', response.status())
-    console.log('response body:', await response.json())
-    expect(response.status()).toBe(StatusCodes.OK )
-});
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.OK)
+})
 
-test ('post credit info with positive decision should receive code 200', async ({ request })  => {
-// prepare request body with dto pattern and medium risk
-    const requestBody = new ApplicationDto(20000, 0, 30, true, 500, 6);
-    const response:APIResponse = await request.post('https://backend.tallinn-learning.ee/api/loan-calc/decision',{
-        data: requestBody
-    });
-    console.log('response status:', response.status())
-    console.log('response body:', await response.json())
-    expect(response.status()).toBe(StatusCodes.OK )
-});
+test('post credit info with positive decision should receive code 200', async ({ request }) => {
+  // prepare request body with dto pattern and medium risk
+  const requestBody = new ApplicationDto(20000, 0, 30, true, 500, 6)
+  const response: APIResponse = await request.post(
+    'https://backend.tallinn-learning.ee/api/loan-calc/decision',
+    {
+      data: requestBody,
+    },
+  )
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.OK)
+})
 
-test ('post credit info with positive decision should receive code 200', async ({ request }) => {
-    // prepare request body with dto pattern and low risk
-    const requestBody = new ApplicationDto(20000, 0, 30, true, 500, 12);
-    const response:APIResponse = await request.post('https://backend.tallinn-learning.ee/api/loan-calc/decision',{
-        data: requestBody
-    });
-    console.log('response status:', response.status())
-    console.log('response body:', await response.json())
-    expect(response.status()).toBe(StatusCodes.OK )
-});
+test('post credit info with positive decision should receive code 200', async ({ request }) => {
+  // prepare request body with dto pattern and low risk
+  const requestBody = new ApplicationDto(20000, 0, 30, true, 500, 12)
+  const response: APIResponse = await request.post(
+    'https://backend.tallinn-learning.ee/api/loan-calc/decision',
+    {
+      data: requestBody,
+    },
+  )
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.OK)
+})
